c94cfce182d6e48a8e320af55b4c9571c111b668
khaledali0907 <khaledali0907@gmail.com>
2026-03-28 15:46:29 +0200

    Update dependencies and implement question navigation in quiz app
    - Added new dependencies in pubspec.lock and pubspec.yaml, including google_fonts.
    - Enhanced QuestionsScreen to display questions and answers with navigation functionality.
    - Implemented answer shuffling in QuizQuestion model for dynamic answer presentation.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/answer_button.dart';
+import 'package:quizz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -8,9 +11,50 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      if (questionIndex < questions.length - 1) {
+        questionIndex++;
+      } else {
+        questionIndex = 0;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Questions Screen'));
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 2,
+          children: [
+            Text(
+              questions[questionIndex].text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            // for (var answer in questions[questionIndex].answers)
+            //   AnswerButton(answerText: answer, onTap: () {}),
+            SizedBox(height: 30),
+            ...questions[questionIndex].getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: answerQuestion,
+              );
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
- 
